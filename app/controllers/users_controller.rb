2f59cf0a192425716dd717642c3714737f0dd80b
Bhavik Patel <Bhavikpatel576@gmail.com>
2017-11-09 07:50:49 -0600

    connect clearnance for authentication. Deleted previous valiations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@
 class UsersController < Clearance::UsersController
     private
 
+    def new
+      @user = User.new
+      render template: "users/new"
+    end
+
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,14 @@
 class User < ApplicationRecord
   include Clearance::User
+  
+  validates :username, presence: true, uniqueness: true 
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, length: { maximum: 255 },
+  #                  format: { with: VALID_EMAIL_REGEX },
+  #                  uniqueness: { case_sensitive: false }
+  #has_secure_password
+  #validates :password, presence: true, length: { minimum: 6}, allow_nil: true
 
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6}, allow_nil: true
-
-  def User.digest(string)
-  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-  	BCrypt::Password.create(string, cost: cost)
-  end
-
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  #Remembers a user for the purpose of a persistent user
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
-
-  #Forgets users -> allows them to log out 
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
-
-  #Returns true if the session matches the digest
-  def authenticated?(remember_token)
-    #not the same remember_token
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
 end
 
 
